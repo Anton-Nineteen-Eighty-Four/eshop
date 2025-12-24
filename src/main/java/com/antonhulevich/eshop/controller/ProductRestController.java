ccd4d126f8af429b320dc87798c4c220be05c373
Anton Hulevich <antonhulevich@gmail.com>
2025-12-24 14:55:16 +0300

    add ProductRestController

---
+++ src/main/java/com/antonhulevich/eshop/controller/ProductRestController.java
@@ -0,0 +1,26 @@
+package com.antonhulevich.eshop.controller;
+
+import com.antonhulevich.eshop.dto.ProductDto;
+import com.antonhulevich.eshop.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductRestController {
+
+    private final ProductService productService;
+
+    public ProductRestController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/{id}")
+    public ProductDto getById(@PathVariable Long id) {
+        return productService.getById(id);
+    }
+
+    @PostMapping
+    public void addProduct(@RequestBody ProductDto dto) {
+        productService.addProduct(dto);
+    }
+}
